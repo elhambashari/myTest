c2f2fcc443364dee6d833b3e48367bf273c06937
Elham Bashari <elhambashari84@gmail.com>
2025-05-21 18:26:38 +0200

    First commit

---
+++ src/exemple.js
@@ -0,0 +1,12 @@
+
+export function sum(a, b) {
+  return a + b;
+}
+
+export function multi(c, d) {
+  return c * d;
+}
+
+export function login(namn, lösenord) {
+  return { namn, lösenord };
+}
---
+++ src/tests/exemple.test.js
@@ -0,0 +1,14 @@
+
+import { sum, multi, login } from '../exemple.js';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('multi c*d', () => {
+  expect(multi(5, 20)).toBe(100);
+});
+
+test('login works', () => {
+  expect(login('elham', '12345')).toEqual({ namn: 'elham', lösenord: '12345' });
+});
