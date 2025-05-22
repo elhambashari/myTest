297af766a408138dadf9167e3b15b95b0f3fd9f5
Elham Bashari <elhambashari84@gmail.com>
2025-05-22 08:29:35 +0200

    exemple changed

--- src/exemple.js
+++ src/exemple.js
@@ -3,13 +3,17 @@ export function sum(a, b) {
   return a + b;
 }
 
+
 export function multi(c, d) {
   return c * d;
 }
 
+
 export function login(namn, lösenord) {
   return { namn, lösenord };
 }
+
+
 export function combin(a,b){
 	return a+b
 }
@@ -18,13 +22,16 @@ export function stor(str) {
   return str.toUpperCase();
 }
 
+
 export function sub(x,y){
 	return (x-y)
 }
 
+
 export function div(n,m){
 	return (n/m)
 }
+
 
 export function storTal(array){
 	return Math.max(...array)
